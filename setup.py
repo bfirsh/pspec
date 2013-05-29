a5a973792121008b85007e1028ec10d1ce0e1ead
Ben Firshman <ben@firshman.co.uk>
2013-05-29 17:38:36 +0100

    0.0.2-dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pspec',
-    version='0.0.1',
+    version='0.0.2-dev',
     description='Python testing for humans',
     author='Ben Firshman',
     author_email='ben@firshman.co.uk',
